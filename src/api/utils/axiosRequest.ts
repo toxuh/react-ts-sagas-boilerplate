50726f4cfeca20e82201eb63089baf803bebb17e
Zakharov Anton <zakharov.toxa@gmail.com>
2020-12-02 16:34:14 +0100

    Axios config fix

--- src/api/utils/axiosRequest.ts
+++ src/api/utils/axiosRequest.ts
@@ -2,7 +2,11 @@ import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
 
 import { BASE_API_URL } from '../constants';
 
-export default ({ url, method }: AxiosRequestConfig): AxiosPromise =>
-  axios({ url: `${BASE_API_URL}${url}`, method }).then(
-    (response) => response.data,
-  );
+export default (config: AxiosRequestConfig): AxiosPromise => {
+  const configWithUrl = {
+    ...config,
+    url: `${BASE_API_URL}${config.url}`,
+  };
+
+  return axios(configWithUrl).then((response) => response.data);
+};
